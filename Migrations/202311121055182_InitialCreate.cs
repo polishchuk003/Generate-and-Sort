7fbf88fa1d54e4ab296b5ba13db1d96ae7ede49d
Serega Polishchuk <serzik2303@gmail.com>
2023-11-14 22:57:53 +0200

    add migration

---
+++ Migrations/202311121055182_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿namespace Brighteye.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.SortedNumbers",
+                c => new
+                    {
+                        ValueId = c.Int(nullable: false, identity: true),
+                        Value = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.ValueId);
+            
+            CreateTable(
+                "dbo.UnsortedNumbers",
+                c => new
+                    {
+                        ValueId = c.Int(nullable: false, identity: true),
+                        Value = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.ValueId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.UnsortedNumbers");
+            DropTable("dbo.SortedNumbers");
+        }
+    }
+}
